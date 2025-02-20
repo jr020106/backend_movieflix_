dad3fb8f9ae856430dc87396f0861f84ec20a723
jr020106 <ratrajatin020106@gmail.com>
2025-02-20 22:40:16 +0530

    first commit

---
+++ routes.py
@@ -0,0 +1,35 @@
+from flask import render_template, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required
+from app import app, db, bcrypt
+from models import User
+from forms import RegistrationForm, LoginForm
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password_hash=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created!', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Check email and password', 'danger')
+    return render_template('login.html', form=form)
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
